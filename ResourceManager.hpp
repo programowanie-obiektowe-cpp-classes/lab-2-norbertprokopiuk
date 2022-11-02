d6a1806c412decdb9c6dd355009ddc62e1171848
norbertprokopiuk <61018774+norbertprokopiuk@users.noreply.github.com>
2022-11-02 23:27:47 +0100

    zaliczenie

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,18 +1,34 @@
 #pragma once
-
 #include "Resource.hpp"
 
 class ResourceManager
 {
 private:
-    Resource resource;
+    Resource* resource;
 
 public:
     // Twoja implementacja tutaj
-    ResourceManager() : resource(Resource{}){}
+    ResourceManager() 
+    {
+        resource = new Resource{};
+    }
 
-    ResourceManager(ResourceManager& a) : resource(a.resource) {}
-    ~ResourceManager() {}
+    ResourceManager(const ResourceManager& a)
+    {
+        resource = new Resource{};
+        *resource = *a.resource;
+    }
+    ~ResourceManager() { 
+        delete resource;
+    }
 
-    double get() { return resource.get(); }
+    double get() { return resource->get(); }
+
+    ResourceManager& operator=(const ResourceManager& w)
+    {
+        delete this->resource;
+        resource = new Resource{};
+        *resource = *w.resource;
+        return *this;
+    }
 };
