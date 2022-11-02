e4ebbe980ca1ac8fddcb5a205d3c0d61bf9e5994
norbertprokopiuk <61018774+norbertprokopiuk@users.noreply.github.com>
2022-11-02 20:15:13 +0100

    test1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,15 @@
 
 class ResourceManager
 {
+private:
+    Resource resource;
+
+public:
     // Twoja implementacja tutaj
+    ResourceManager() : resource(Resource{}){}
+
+    ResourceManager(ResourceManager& a) : resource(a.resource) {}
+    ~ResourceManager() {}
+
+    double get() { return resource.get(); }
 };
