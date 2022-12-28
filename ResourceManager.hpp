89a5ffad23356bc82485f11af6e26718c9c76d51
norbertprokopiuk <61018774+norbertprokopiuk@users.noreply.github.com>
2022-12-28 20:15:23 +0100

    zaliczenie zad 3

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -8,27 +8,37 @@ private:
 
 public:
     // Twoja implementacja tutaj
-    ResourceManager() 
-    {
-        resource = new Resource{};
-    }
+    ResourceManager() { resource = new Resource{}; }
 
     ResourceManager(const ResourceManager& a)
     {
-        resource = new Resource{};
+        resource  = new Resource{};
         *resource = *a.resource;
     }
-    ~ResourceManager() { 
-        delete resource;
+
+    ResourceManager(ResourceManager&& a)
+    {
+        resource  = a.resource;
+        a.resource = nullptr;
     }
+
+    ~ResourceManager() { delete resource; }
 
     double get() { return resource->get(); }
 
     ResourceManager& operator=(const ResourceManager& w)
     {
         delete this->resource;
-        resource = new Resource{};
+        resource  = new Resource{};
         *resource = *w.resource;
+        return *this;
+    }
+
+    ResourceManager& operator=(ResourceManager&& w)
+    {
+        delete this->resource;
+        resource  = w.resource;
+        w.resource = nullptr;
         return *this;
     }
 };
